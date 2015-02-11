b7ff1822042711601efa59ab47b02d87b9ce8078
Mark Burnstein <mark.burnstein@gmail.com>
2015-02-11 15:33:03 -0800

    test loopback.php with  and  html pages

--- src/loopback.php
+++ src/loopback.php
@@ -1,47 +1,10 @@
 <?php
   error_reporting(E_ALL);
   ini_set('display-errors', 'On');
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>LoopBack</title>
-</head>
-<body>
-  <h2>Key / Value pairs</h2>
-
-  <form method="GET" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   Enter: <input type="text" name="key">
-   <br><br>
-   Enter: <input type="text" name="key2">
-   <br><br>
-   Enter: <input type="text" name="key23">
-   <br><br>
-   Enter: <input type="text" name="help">
-   <br><br>
-   <input type="submit" value="get"> 
- </form>
- <br>
-
-
- <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   Enter: <input type="text" name="key">
-   <br><br>
-   Enter: <input type="text" name="key2">
-   <br><br>
-   Enter: <input type="text" name="key23">
-   <br><br>
-   Enter: <input type="text" name="help">
-   <br><br>
-   <input type="submit" value ="post"> 
- </form>
- <br>
-
-<?php
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	if (empty($_POST)) {
-		$arr = array("Type" => "POST", "parameters" => "NULL");
+	if (empty($_GET)) {
+		$arr = array("Type" => "GET", "parameters" => "NULL");
 	} else {
   $arr = array("Type" => "GET", "parameters" => $_GET);
 }
